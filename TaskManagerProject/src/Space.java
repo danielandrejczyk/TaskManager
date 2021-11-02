f2aa31c6294f146ab03c84b54c8ea71c2e2f21b9
tteper <tteper@Dell-XPS-15>
2021-11-01 21:01:28 -0500

    Added basic class fields and methods.

--- TaskManagerProject/src/Space.java
+++ TaskManagerProject/src/Space.java
@@ -1,5 +1,62 @@
-
+/**
+ * 
+ * @author tteper
+ * 
+ * TaskManager Space class
+ *
+ */
 public class Space {
-	// @author: Tom
-	// this is a test
+	
+	private Space parentSpace;
+	private String name;
+	
+	/**
+	 * Default constructor
+	 * @param name the name of space to be created
+	 */
+	public Space(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Special constructor
+	 * @param parent the name of the target parent space
+	 * @param name the name of space to be created
+	 */
+	public Space(Space parent, String name) {
+		this.parentSpace = parent;
+		this.name = name;
+	}
+	
+	/**
+	 * Setter method for modifying Space name
+	 * @param n the name of the space to be modified
+	 */
+	public void SetName(String n) {
+		this.name = name;
+	}
+	
+	/**
+	 * Getter method for retrieving Space name
+	 * @return String name of space
+	 */
+	public String GetName() {
+		return name;
+	}
+	
+	/**
+	 * Method to change the parent space of this space
+	 * @param aSpace the target space
+	 */
+	public void MoveTo(Space aSpace) {
+		parentSpace = aSpace;
+	}
+	
+	/**
+	 * Getter method for retrieving parent Space name
+	 * @return String name of parent space
+	 */
+	public String GetParentName() {
+		return parentSpace.GetName();
+	}
 }
