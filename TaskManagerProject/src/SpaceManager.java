95b70f32a6a89b2a437bea1c4aff7e794c1ca2f9
tteper <tteper@Dell-XPS-15>
2021-11-01 21:08:54 -0500

    Added basic fields and methods to SpaceManager class

--- TaskManagerProject/src/SpaceManager.java
+++ TaskManagerProject/src/SpaceManager.java
@@ -3,8 +3,35 @@
  * 
  * This class is for Space Manager
  */
-
+import java.util.ArrayList;
 
 public class SpaceManager {
 	
+	private ArrayList<Space> spaceList;
+	
+	/**
+	 * Default constructor for SpaceManager class
+	 * 
+	 * Creates a default My Tasks space and adds to list of spaces
+	 */
+	public SpaceManager() {
+		Space MY_TASKS = new Space("My Tasks");
+		spaceList.add(MY_TASKS);
+	}
+	
+	public void AddSpace(Space aParent) {
+	
+	}
+	
+	public void EditSpace(Space aSpace) {
+		
+	}
+	
+	public void DeleteSpace(Space aSpace) {
+		
+	}
+	
+	public void SelectSpace() {
+		
+	}
 }
