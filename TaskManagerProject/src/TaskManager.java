1047baa6d4084e884673a5fdcfc996be7a9eef33
Zachary Calen Stewart <zcs0004@uah.edu>
2021-11-04 13:43:55 -0500

    SpaceManager class

---
+++ TaskManagerProject/src/TaskManager.java
@@ -0,0 +1,49 @@
+/**
+*
+* @author calen4
+*
+*/
+import java.util.ArrayList;
+
+public class TaskManager {
+	
+	private ArrayList<Task> taskList = new ArrayList<Task>();
+	
+	/**
+	 * Default constructor
+	 */
+	public TaskManager() {
+		
+	}
+	
+	/**
+	 * Method to add a task to a space
+	 * @param aParent the space that will contain the task
+	 */
+	public void AddTask(Space aParent) {
+	
+	}
+	
+	/**
+	 * Method to edit a task
+	 * @param aTask the task that will be edited
+	 */
+	public void EditTask(Task aTask) {
+		
+	}
+	
+	/**
+	 * Method to delete a task
+	 * @param aTask the task that will be deleted
+	 */
+	public void DeleteTask(Task aTask) {
+		
+	}
+	
+	/**
+	 * Method to select a task
+	 */
+	public void SelectTask() {
+		
+	}
+}
