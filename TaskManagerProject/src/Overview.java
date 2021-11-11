5a0fc98fed3b8f96a581353f73779685e2dc8638
Daniel Andrejczyk <andrejczykd@gmail.com>
2021-11-11 17:33:06 -0600

    Added top panel with buttons for editting and deleting tasks and spaces. Also added a space combination box. Need to add event handling and fix the space's tostring method in Space.java.

--- TaskManagerProject/src/Overview.java
+++ TaskManagerProject/src/Overview.java
@@ -1,21 +1,62 @@
 // Author: Daniel Andrejczyk
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
  
 public class Overview extends Application {
+	
     public static void main(String[] args) {
         launch(args);
     }
     
+    /**
+     * Overrides start from Application class
+     * to set-up JavaFX stage, scene, and scene graph.
+     * 
+     * @param	primaryStage	The primary stage to build on.
+     */
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
+    	
+    	// Set title
+        primaryStage.setTitle("Task Manager");
+        
+        BorderPane border = new BorderPane();
+        
+        // Top panel
+        HBox topSection = new HBox();
+        topSection.setPadding(new Insets(15, 12, 15, 12));
+        topSection.setSpacing(10);
+        
+        Button editSpace = new Button("Edit Space");
+        editSpace.setPrefSize(40, 40);
+        
+        Button deleteSpace = new Button("Delete Space");
+        deleteSpace.setPrefSize(40, 40);
+        
+        ComboBox<Space> spaceFilter = new ComboBox<Space>();
+        spaceFilter.getItems().addAll(
+        		new Space("My Tasks")
+        );
+        
+        Button editTask = new Button("Edit Task");
+        editSpace.setPrefSize(40, 40);
+        
+        Button deleteTask = new Button("Delete Task");
+        deleteSpace.setPrefSize(40, 40);
+        
+        topSection.getChildren().addAll(editSpace, deleteSpace, spaceFilter, editTask, deleteTask);
+        
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
@@ -27,8 +68,10 @@ public class Overview extends Application {
         });
         
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        root.getChildren().add(topSection);
+        
         primaryStage.setScene(new Scene(root, 300, 250));
+        // Show the scene
         primaryStage.show();
     }
 }
