14b9fbaeaef21a69411fafdae32245c7676fae8e
Zachary Calen Stewart <zcs0004@uah.edu>
2021-11-11 14:49:03 -0600

    Task class basic variable and method definitions

--- TaskManagerProject/src/Task.java
+++ TaskManagerProject/src/Task.java
@@ -1,11 +1,47 @@
 
 public class Task {
 
-	// Daniel Andrejczyk
-	// Tom
-	// Test
-	
 	// Justin Wilson
 	
 	// calen 2
+	
+	 private String name;
+	 private Date duedate;
+	 private enum Priority
+	 {
+		 LOW, MEDIUM, HIGH;
+	 }
+	 private Priority currentPriority = Priority.MEDIUM;
+	 private Space parentSpace;
+	 public Task(String n, Date dd){
+		 
+	 }
+	 public Task(String n, Date dd, Space aSpace){
+		 
+	 }
+	 public void SetName(String n){
+		 
+	 }
+	 public String GetName(){
+		 
+	 }
+	 public void SetDate(Date d){
+		 
+	 }
+	 public String GetDate(){
+		 
+	 }
+	 public void SetPriority(Priority p){
+		 
+	 }
+	 public Priority GetPriority(){
+		 
+	 }
+	 public void MoveTo(Space aSpace){
+		 
+	 }
+	 public String GetParentName(){
+		 
+	 }
+
 }
