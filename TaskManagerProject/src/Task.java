2b4eac13808262911c79e5e56f4d5fa9d335e52e
Zachary Calen Stewart <zcs0004@uah.edu>
2021-11-17 20:56:14 -0600

    methods in task / taskmanager

--- TaskManagerProject/src/Task.java
+++ TaskManagerProject/src/Task.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 
 public class Task {
 
@@ -7,41 +8,65 @@ public class Task {
 	
 	 private String name;
 	 private Date duedate;
+	 
+	 private Priority currentPriority = Priority.MEDIUM;
+	 private Space parentSpace;
+	 
 	 private enum Priority
 	 {
 		 LOW, MEDIUM, HIGH;
 	 }
-	 private Priority currentPriority = Priority.MEDIUM;
-	 private Space parentSpace;
+	 
 	 public Task(String n, Date dd){
-		 
+		 this.name = n;
+		 this.duedate = dd;
 	 }
+	 
+	 
 	 public Task(String n, Date dd, Space aSpace){
-		 
+		 this.name = n;
+		 this.duedate = dd;
+		 this.parentSpace = aSpace;
 	 }
+	 
+	 
 	 public void SetName(String n){
-		 
+		 this.name = n;
 	 }
+	 
+	 
 	 public String GetName(){
-		 
+		 return name;
 	 }
-	 public void SetDate(Date d){
-		 
+	 
+	 
+	 public void SetDate(Date dd){
+		 this.duedate = dd;
 	 }
-	 public String GetDate(){
-		 
+	 
+	 
+	 public Date GetDate(){
+		 return duedate;
 	 }
+	 
+	 
 	 public void SetPriority(Priority p){
-		 
+		 currentPriority = p;
 	 }
+	 
+	 
 	 public Priority GetPriority(){
-		 
+		 return currentPriority;
 	 }
+	 
+	 
 	 public void MoveTo(Space aSpace){
-		 
+		 parentSpace = aSpace;
 	 }
+	 
+	 
 	 public String GetParentName(){
-		 
+		 return parentSpace.toString();
 	 }
 
 }
