aecaba68f92a558e306e18ea05288bb192c5f1c6
jayro <jayro@10.0.0.207>
2021-11-21 11:26:12 -0600

    Task Methods mostly commented out

--- TaskManagerProject/src/Task.java
+++ TaskManagerProject/src/Task.java
@@ -19,8 +19,11 @@ public class Task {
 	private Priority currentPriority = Priority.MEDIUM;
 	private Space parentSpace;
 	
-	/*
-	 * Constructors
+	/**
+	 * Default constructor for Task Class
+	 * creates a task object with a name, due date, and a new status 
+	 * @param n the name of the task
+	 * @param dd the due date of task
 	 */
 	
 	public Task(String n, Date dd)
@@ -30,6 +33,14 @@ public class Task {
 		status = new Status();
 	}
 	
+	/**
+	 * Parameterized constructor for Task Class
+	 * creates a task object with a name, due date, and parent space to belong to with a new status
+	 * @param n the name of the task
+	 * @param dd the due date of task
+	 * @param aSpace the parent of space of the new task
+	 */
+	
 	public Task(String n, Date dd, Space aSpace)
 	{
 		name = n;
@@ -38,10 +49,19 @@ public class Task {
 		status = new Status();
 	}
 	
+	/**
+	 * Setter method for modifying Task name
+	 * @param n the name of the task to be modified
+	 */
+	
 	public void setName(String n){
 		name = n;
 	}
 	
+	/**
+	 * Getter method for retrieving Task name
+	 * @return String name of task
+	 */	
 	@Override
 	public String toString() {
 		return name;
@@ -56,17 +76,38 @@ public class Task {
 		return dueDate;
 	}
 	
+	/**
+	 * Setter method for changing the priority of the task
+	 * @param p the priority that will be modified
+	 */
+	
 	public void setPriority(Priority p){
 		currentPriority = p;
 	}
+	
+	/**
+	 * Getter method for retrieving the priority of task
+	 * @return priority of the task
+	 */	
 	
 	public Priority getPriority(){
 		return currentPriority;
 	}
 	
+	/**
+	 * Method to change the parent space of this task
+	 * @param aSpace the target space
+	 */
+	
 	public void moveTo(Space aSpace){
 		parentSpace = aSpace;
 	}
+	
+	/**
+	 * Getter method for retrieving parent Space name 
+	 * that the task belongs to
+	 * @return String name of parent space
+	 */
 	
 	public String getParentName(){
 		return parentSpace.toString();
